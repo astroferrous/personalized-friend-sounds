799c35404fa17ee5f034fd0a72881d26fe7e49e8
Astro <thecoloradoathlete@gmail.com>
2026-04-08 16:43:34 -0700

    its alive

--- src/main/java/com/personalizedfriendsounds/PersonalizedFriendSoundEngine.java
+++ src/main/java/com/personalizedfriendsounds/PersonalizedFriendSoundEngine.java
@@ -1,61 +1,33 @@
 package com.personalizedfriendsounds;
 
-import lombok.extern.slf4j.Slf4j;
-
-import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.sound.sampled.*;
-import java.io.File;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
-@Slf4j
 @Singleton
 public class PersonalizedFriendSoundEngine
 {
-    private final PersonalizedFriendSoundsConfig config;
-
-    @Inject
-    public PersonalizedFriendSoundEngine(PersonalizedFriendSoundsConfig config)
-    {
-        this.config = config;
-    }
-
     public void playClip(String soundFile)
             throws UnsupportedAudioFileException, IOException, LineUnavailableException
     {
-        File file = new File(soundFile);
-        if (!file.exists())
+        String resourcePath = "/sounds/" + soundFile;
+
+        try (InputStream rawStream = getClass().getResourceAsStream(resourcePath))
         {
-            throw new IOException("Sound file not found: " + soundFile);
-        }
-
-        try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file))
-        {
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-
-            int volume = Math.max(0, Math.min(100, config.pluginVolume()));
-            FloatControl gainControl = null;
-
-            if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN))
+            if (rawStream == null)
             {
-                gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-                float min = gainControl.getMinimum();
-                float max = gainControl.getMaximum();
-
-                float gain;
-                if (volume == 0)
-                {
-                    gain = min;
-                }
-                else
-                {
-                    gain = min + (max - min) * (volume / 100f);
-                }
-                gainControl.setValue(gain);
+                throw new IOException("Sound resource not found: " + resourcePath);
             }
 
-            clip.start();
+            try (BufferedInputStream bufferedStream = new BufferedInputStream(rawStream);
+                 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedStream))
+            {
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+            }
         }
     }
 }

9c4dade6f1f9a32f600cf6eae2f974727a498399
Astro <thecoloradoathlete@gmail.com>
2026-04-13 12:49:34 -0700

    sound engine fixes + pathing

--- src/main/java/com/personalizedfriendsounds/PersonalizedFriendSoundEngine.java
+++ src/main/java/com/personalizedfriendsounds/PersonalizedFriendSoundEngine.java
@@ -1,68 +1,65 @@
 package com.personalizedfriendsounds;
 
-import lombok.extern.slf4j.Slf4j;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
-import java.io.BufferedInputStream;
-import java.io.InputStream;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.audio.AudioPlayer;
 
 @Slf4j
 @Singleton
 public class PersonalizedFriendSoundEngine
 {
     private final PersonalizedFriendSoundsConfig config;
+    private final AudioPlayer audioPlayer;
 
     @Inject
-    public PersonalizedFriendSoundEngine(PersonalizedFriendSoundsConfig config)
+    public PersonalizedFriendSoundEngine(
+            PersonalizedFriendSoundsConfig config,
+            AudioPlayer audioPlayer
+    )
     {
         this.config = config;
+        this.audioPlayer = audioPlayer;
     }
 
-    public void playClip(String soundFile) throws Exception
+    public void playClip(String soundFile)
     {
-        try (InputStream rawStream = PersonalizedFriendSoundFileManager.getSoundStream(soundFile);
-             BufferedInputStream bufferedStream = new BufferedInputStream(rawStream);
-             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedStream))
+        float gainDb = toGainDb(config.pluginVolume());
+
+        try
         {
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
+            PersonalizedFriendSoundFileManager.SoundHandle soundHandle =
+                    PersonalizedFriendSoundFileManager.resolveSound(soundFile);
 
-            applyVolume(clip);
-
-            clip.start();
-
-            new Thread(() ->
+            if (soundHandle == null)
             {
-                try
-                {
-                    Thread.sleep(clip.getMicrosecondLength() / 1000);
-                }
-                catch (InterruptedException ignored) {}
+                log.warn("Could not resolve sound '{}'", soundFile);
+                return;
+            }
 
-                clip.close();
-            }).start();
+            if (soundHandle.isExternalFile())
+            {
+                audioPlayer.play(soundHandle.getFile(), gainDb);
+            }
+            else
+            {
+                audioPlayer.play(
+                        PersonalizedFriendSoundEngine.class,
+                        soundHandle.getResourcePath(),
+                        gainDb
+                );
+            }
+        }
+        catch (Exception e)
+        {
+            log.warn("Failed to play sound '{}'", soundFile, e);
         }
     }
 
-    private void applyVolume(Clip clip)
+    private float toGainDb(int volumePercent)
     {
-        if (!clip.isControlSupported(FloatControl.Type.MASTER_GAIN))
-        {
-            return;
-        }
-
-        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        int volume = Math.max(0, Math.min(100, config.pluginVolume()));
-
-        float min = gainControl.getMinimum();
-        float max = gainControl.getMaximum();
-        float gain = (volume == 0) ? min : (min + (max - min) * (volume / 100.0f));
-
-        gainControl.setValue(gain);
+        int clamped = Math.max(1, Math.min(100, volumePercent));
+        float volume = clamped / 100f;
+        return (float) Math.log10(volume) * 20f;
     }
 }
